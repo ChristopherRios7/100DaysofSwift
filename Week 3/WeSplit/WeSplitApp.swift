e5a5ba95b850c2b9296ad867471521fb753994da
Christopher Rios <Christopher.Rios8@outlook.com>
2023-08-29 09:36:14 -0500

    Changes

---
+++ Week 3/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Christopher Rios on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
