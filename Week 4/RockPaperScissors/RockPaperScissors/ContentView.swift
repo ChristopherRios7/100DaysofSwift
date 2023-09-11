4ab246732b243df835e434db8e49641bc334e421
Christopher Rios <Christopher.Rios8@outlook.com>
2023-09-11 17:18:48 -0500

    BetterRest Part 2
    Pushing code from day 27, BetterRest part two complete.

--- Week 4/RockPaperScissors/RockPaperScissors/ContentView.swift
+++ Week 4/RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -16,39 +16,54 @@ struct ContentView: View {
     @State private var showingResults = false
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text("The computer has played...")
-                .font(.headline)
-            Text(moves[computerChoice])
-                .font(.system(size:200))
-            if shouldWin {
-                Text("Which One Wins?")
-                    .foregroundStyle(.green)
-                    .font(.title)
-            } else {
-                Text("Which One Loses?")
-                    .foregroundStyle(.red)
-                    .font(.title)
-            }
-            HStack{
-                ForEach(0..<3){ number in
-                    Button(moves[number]){
-                    play(choice: number)
-                    }
-                    .font(.system(size: 80))
-                    
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.25, green: 0.25, blue: 0.75), location: 0.3),
+                .init(color: Color(red: 0.25, green: 0.55, blue: 0.15), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("The computer has played...")
+                    .font(.headline)
+                Text(moves[computerChoice])
+                    .font(.system(size:200))
+                    .background(.regularMaterial)
+                    .padding(.vertical, 20)
+                    .clipShape(Circle())
+                if shouldWin {
+                    Text("Which One Wins?")
+                        .foregroundStyle(.green)
+                        .font(.title)
+                } else {
+                    Text("Which One Loses?")
+                        .foregroundStyle(.red)
+                        .font(.title)
                 }
+                HStack{
+                    ForEach(0..<3){ number in
+                        Button(moves[number]){
+                            play(choice: number)
+                        }
+                        .font(.system(size: 80))
+                        
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 35))
+                Spacer()
+                Text("Score: \(score)")
+                    .font(.headline)
+                Spacer()
             }
-            Spacer()
-            Text("Score: \(score)")
-                .font(.subheadline)
-            Spacer()
-        }
-        .alert("Game Over", isPresented: $showingResults){
-            Button("Play again?", action: reset)
-        }message: {
-            Text("Your score is: \(score).")
+            .alert("Game Over", isPresented: $showingResults){
+                Button("Play again?", action: reset)
+            }message: {
+                Text("Your score is: \(score).")
+            }
         }
     }
     
