4c4b2a2602668d0ce7562cf672558569a63681c3
Christopher Rios <Christopher.Rios8@outlook.com>
2023-09-21 20:41:52 -0500

    Project 7 push

---
+++ Week 5/Project 7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Christopher Rios on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
