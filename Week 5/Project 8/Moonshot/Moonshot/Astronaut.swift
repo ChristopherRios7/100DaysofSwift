692ed7d0963fb62bf73120061d4a550efd9d8f2f
Christopher Rios <Christopher.Rios8@outlook.com>
2023-09-25 19:56:47 -0500

    Project 8 part 3 push

---
+++ Week 5/Project 8/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Christopher Rios on 9/24/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
