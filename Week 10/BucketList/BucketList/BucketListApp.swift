1c366e278f70b88acc87e4c88c247c2aabed23f2
Christopher Rios <Christopher.Rios8@outlook.com>
2023-10-24 10:49:50 -0500

    Day 70!

---
+++ Week 10/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Christopher Rios on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
