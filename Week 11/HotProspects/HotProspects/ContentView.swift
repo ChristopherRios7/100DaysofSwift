abe290945b75e53eb3c9f4120024ee37f602aeb1
Christopher Rios <Christopher.Rios8@outlook.com>
2023-11-04 09:31:02 -0500

    Reset
    Beginning implementation phase tomorrow

--- Week 11/HotProspects/HotProspects/ContentView.swift
+++ Week 11/HotProspects/HotProspects/ContentView.swift
@@ -6,37 +6,13 @@
 //
 
 import SwiftUI
-import SamplePackage
 import UserNotifications
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Button("Request Permission") {
-                //first
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All Set!")
-                    } else if let error = error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            
-            Button("Schedule Notification") {
-                //second
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the dogs"
-                content.subtitle = "They look hungry"
-                content.sound = UNNotificationSound.default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-            }
-        }
+        Text("Hello, World!")
+            .padding()
     }
 }
     
