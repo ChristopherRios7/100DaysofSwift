33a3d8f6c0e996965f6ad2a767bc33dc86d06b56
Christopher Rios <Christopher.Rios8@outlook.com>
2023-11-14 19:23:02 -0600

    Day 91 push

---
+++ Week 12/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Christopher Rios on 11/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in CGFloat(position) * -10 }
+            }
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
+    }
+}
+
+#Preview {
+    ContentView()
+}
