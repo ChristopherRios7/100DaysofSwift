e19422cb12b0246c8dc2cdb5d2dcfd5a05916e51
Christopher Rios <Christopher.Rios8@outlook.com>
2023-11-19 10:42:46 -0600

    Importing Resources

--- Week 13/SnowSeeker/SnowSeeker/ContentView.swift
+++ Week 13/SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,24 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var searchText = ""
-    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
     
     var body: some View {
         NavigationView {
-            List(filteredNames, id: \.self) { name in
-                Text(name)
-            }
-            .searchable(text: $searchText, prompt: "Look for something")
-            .navigationTitle("Searching")
-        }
-    }
-    
-    var filteredNames: [String] {
-        if searchText.isEmpty {
-            return allNames
-        } else {
-            return allNames.filter { $0.contains(searchText) }
+            Text("Hello, World!")
         }
     }
 }
